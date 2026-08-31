8350d9c046fda21fc0fcbb4050d6ac4af5962ff5
shubamc04-code <shubamc04@gmail.com>
2026-08-31 21:19:03 +0530

    Update Auth import path from Auth.js to Auth

--- client/src/components/AuthModel.jsx
+++ client/src/components/AuthModel.jsx
@@ -1,7 +1,7 @@
 import { useSelector } from "react-redux";
 import { useEffect } from "react";
 import { FaTimes } from "react-icons/fa";
-import Auth from "../pages/Auth";
+import Auth from "../pages/Auth.js";
 
 function AuthModel({ onClose }) {
 
